7c8faa96d93f75fb4263d9a4ab74d075ce2e1195
Roman Shelekhov <shelekhoff@gmail.com>
2025-05-02 01:00:17 +0400

    Refactor getJWK to use context and improve cache handling; removed updateJWKS function for cleaner logic

--- jwks.go
+++ jwks.go
@@ -1,24 +1,24 @@
 package jwtauth
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
-	"github.com/rshelekhov/jwtauth/cache"
-	"net/http"
 	"time"
+
+	"github.com/rshelekhov/jwtauth/cache"
 )
 
 // JWK represents a JSON Web Key structure containing the necessary fields
 // for RSA public key construction
 type JWK struct {
-	Alg string `json:"alg,omitempty"` // The specific cryptographic algorithm used with the key.
-	Kty string `json:"kty,omitempty"` // The family of cryptographic algorithms used with the key.
+	Alg string `json:"alg,omitempty"` // The specific cryptographic algorithm used with the key
+	Kty string `json:"kty,omitempty"` // The family of cryptographic algorithms used with the key
 	Use string `json:"use,omitempty"` // How the key was meant to be used; sig represents the signature
-	Kid string `json:"kid,omitempty"` // The unique identifier for the key.
+	Kid string `json:"kid,omitempty"` // The unique identifier for the key
 
 	// For RSA keys
 	N string `json:"n,omitempty"` // The modulus for the RSA public key
-	E string `json:"e,omitempty"` // The exponent for the RSA public key.
+	E string `json:"e,omitempty"` // The exponent for the RSA public key
 }
 
 // JWKSResponse represents the structure of the JWKS endpoint response
@@ -31,87 +31,44 @@ const JWKS = "jwks"
 
 // getJWK retrieves a JWK by its key ID (kid) from the cache or fetches new JWKS if needed
 // Returns the matching JWK or an error if not found
-func (m *manager) getJWK(appID, kid string) (*JWK, error) {
+func (m *manager) getJWK(ctx context.Context, appID, kid string) (*JWK, error) {
 	const op = "jwt.manager.getJWK"
 
 	// Construct cache key using appID
 	cacheKey := createCacheKey(appID)
 
-	jwks, found := m.getCachedJWKS(cacheKey)
-	if !found {
-		if err := m.updateJWKS(appID); err != nil {
-			return nil, fmt.Errorf("%s: failed to update JWKS: %w", op, err)
-		}
-	}
-
-	// If not in cache or cache miss, fetch new JWKS from URL
-	if len(jwks) == 0 {
-		if err := m.updateJWKS(appID); err != nil {
+	cachedJWKS, found := m.getCachedJWKS(cacheKey)
+	if !found || len(cachedJWKS) == 0 {
+		jwks, err := m.jwksProvider.GetJWKS(ctx, appID)
+		if err != nil {
 			return nil, fmt.Errorf("%s: failed to update JWKS: %w", op, err)
 		}
 
-		jwks, found = m.getCachedJWKS(cacheKey)
-		if !found {
-			return nil, fmt.Errorf("%s: JWKS not found after update", op)
+		if len(jwks.Keys) == 0 {
+			return nil, fmt.Errorf("%s: no JWKS found for appID %s", op, appID)
 		}
+
+		ttl := cache.DefaultExpiration
+		if jwks.TTL > 0 {
+			ttl = jwks.TTL
+		}
+
+		m.mu.Lock()
+		m.jwksCache.Set(cacheKey, jwks.Keys, ttl)
+		m.mu.Unlock()
+
+		// Update cachedJWKS with new data
+		cachedJWKS = jwks.Keys
 	}
 
 	// Find the JWK with the matching key ID
-	for _, jwk := range jwks {
+	for _, jwk := range cachedJWKS {
 		if jwk.Kid == kid {
 			return &jwk, nil
 		}
 	}
 
 	return nil, fmt.Errorf("%s: JWK with kid %s not found", op, kid)
-}
-
-// updateJWKS fetches fresh JWKS from the configured URL and updates the cache
-// Returns an error if the fetch or update fails
-func (m *manager) updateJWKS(appID string) error {
-	const op = "jwt.manager.updateJWKS"
-
-	if m.jwksURL == "" {
-		return fmt.Errorf("%s: jwksURL is not configured for JWT Manager", op)
-	}
-
-	client := &http.Client{}
-
-	req, err := http.NewRequest(http.MethodGet, m.jwksURL, nil)
-	if err != nil {
-		return fmt.Errorf("%s: failed to create HTTP request: %w", op, err)
-	}
-
-	req.Header.Add(AppIDHeader, appID)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("%s: failed to fetch JWKS: %w", op, err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("%s: failed to fetch JWKS: status code %d", op, resp.StatusCode)
-	}
-
-	var jwks JWKSResponse
-	if err = json.NewDecoder(resp.Body).Decode(&jwks); err != nil {
-		return fmt.Errorf("%s: failed to decode JWKS response: %w", op, err)
-	}
-
-	// Construct cache key using appID
-	cacheKey := createCacheKey(appID)
-
-	ttl := cache.DefaultExpiration
-	if jwks.TTL > 0 {
-		ttl = jwks.TTL
-	}
-
-	m.mu.RLock()
-	m.jwksCache.Set(cacheKey, jwks.Keys, ttl)
-	m.mu.RUnlock()
-
-	return nil
 }
 
 // getCachedJWKS returns a cached JWKS from the cache or nil if not found
